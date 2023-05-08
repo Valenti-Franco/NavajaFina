3692023699f4e0a1efd9d6928277f678a89a57a2
Valenti-Franco <valentifranco23@gmail.com>
2023-05-08 20:40:28 -0300

    oficial navajafina

---
+++ hair/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import  style  from './index.module.css';
+
+const Header = () => {
+  return (
+    <div className={style.header}>NAVAJAFINA</div>
+  )
+}
+
+export default Header
