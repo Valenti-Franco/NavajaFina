c4413e777b1d5c5f312d46a923a83519133c8c08
Valenti-Franco <valentifranco23@gmail.com>
2023-05-11 15:13:55 -0300

    slider completo

--- hair/src/components/Main/Main.js
+++ hair/src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  style  from './index.module.css';
 import ProductCard from '../ProductCard/ProductCard';
 
@@ -7,61 +7,109 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/free-mode";
+
 import "swiper/css/pagination";
 
 
 
 // import required modules
-import { FreeMode, Pagination } from "swiper";
+import {  Pagination, Autoplay, Navigation  } from "swiper";
 
 
-const products = [
-                    {_id:'123163',
-                        name:'Kit Máquina Finisher + Set Tijeras ',
-                        images:'https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg'},
-                    {_id:'12314',
-                      name:'Plancha HCT',
-                      images:'https://www.barbershop.com.ar/product_images/k/881/15478__51429_std.jpg'},
-
-                    {_id:'1236515',
-                    name:'Set de herramientas Profesionales',
-                    images:'https://www.barbershop.com.ar/product_images/v/147/c1534__83513_std.jpg'},
-
-                    {_id:'123165465',
-                    name:'Kit Tijeras',
-                    images:'https://www.barbershop.com.ar/product_images/j/017/695033__95893_std.jpg'},
-
-                    {_id:'1236464615',
-                    name:'Tijera Corte y Pulir Navakin',
-                    images:'https://www.barbershop.com.ar/product_images/q/447/c345__52643_std.jpg'},
-
-                    {_id:'12365464645515',
-                    name:'Set Herraminetas Tijera de Corte',
-                    images:'https://www.barbershop.com.ar/product_images/p/851/c5428__38713_std.jpg'},
-
-                  ];
-
-console.log(products[0].images[0])
 const Main = () => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
+  
+  const products = [
+                      {_id:'123163',
+                          name:'B-Way Five',
+                          images:['https://http2.mlstatic.com/D_NQ_NP_665122-MLA50969950919_082022-O.webp','https://http2.mlstatic.com/D_NQ_NP_722620-MLA50970122347_082022-O.webp'],
+                          price:'$32.900',
+                      },
+                          {_id:'12314',
+                        name:'Cortadora Pelo Profesional Babyliss Big Shot',
+                        images:['https://http2.mlstatic.com/D_NQ_NP_665683-MLA31429871436_072019-O.webp','https://http2.mlstatic.com/D_NQ_NP_795993-MLA31429890111_072019-O.webp'],
+                        price:'$32.900',
+                      },
+  
+                      {_id:'1236515',
+                      name:'Set de herramientas Profesionales',
+                      images:['https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg','https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg'],
+                      price:'$32.900',
+                      },
+  
+                      {_id:'123165465',
+                      name:'Kit Tijeras',
+                      images:['https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg','https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg'],
+                      price:'$32.900',
+                      },
+  
+                      {_id:'1236464615',
+                      name:'Tijera Corte y Pulir Navakin',
+                      images:['https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg','https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg'],
+                      price:'$32.900',
+                      },
+  
+                      {_id:'12365464645fsdf515',
+                      name:'Set Herraminetas Tijera de Corte',
+                      images:['https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg','https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg'],
+                      price:'$32.900',
+                      },
+                      {_id:'123654646sdasd45515',
+                      name:'Set Herraminetas Tijera de Corte',
+                      images:['https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg','https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg'],
+                      price:'$32.900',
+                      },
+                      {_id:'12365464645423423515',
+                      name:'Set Herraminetas Tijera de Corte',
+                      images:['https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg','https://www.barbershop.com.ar/product_images/z/745/528186__74084_std.jpg'],
+                      price:'$32.900',
+                      },
+                      
+  
+                    ];
+  
+  
+      const [selectedSlide, setSelectedSlide] = useState(null);
+    
+      const handleSlideClick = (index) => {
+        console.log(index)
+        setSelectedSlide(index);
+      };
+    
+      const handleSlideBlur = () => {
+        setSelectedSlide(null);
+      };
+  
+                
   return (
     <div className={style.main}>
         <div className={style.containerPeinados}>
 
-            <p>CORTES</p>
+            <h1 className={style.title}>PRODCUTOS</h1>
         <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
+        slidesPerView={4}
+        spaceBetween={170}
+        centeredSlides={true}
         className={style.mySwiper}
+        modules={[Autoplay, Pagination, Navigation]} // Agrega el módulo Pagination
+        pagination={{ clickable: true }} // Configura la paginación
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
       >
         {products.map((product) => (
           
-             <SwiperSlide className={style.swiperSlide} key={product._id}> <ProductCard name={product.name} image={product.images} /></SwiperSlide>
+             <SwiperSlide className={style.swiperSlide + (selectedSlide === product._id ? ' ' + style.selectedSlide : '')}
+            onMouseEnter={() => handleSlideClick(product._id)}
+            onMouseLeave={handleSlideBlur}
+            tabIndex={0}
+             key={product._id}> <ProductCard name={product.name} price={product.price} image={product.images} /></SwiperSlide>
             ))}
        
       </Swiper>
--- hair/src/components/ProductCard/ProductCard.js
+++ hair/src/components/ProductCard/ProductCard.js
@@ -1,16 +1,44 @@
 
 import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
 import  style  from './index.module.css';
+import "swiper/css";
+import "swiper/css/navigation";
+import { FaCartPlus } from 'react-icons/fa';
+import { FaRegEye } from 'react-icons/fa';
 
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 const ProductCard = (product) => {
-    console.log(product)
-    console.log(product.name)
-    console.log(product.image)
-
+    
     return (
     <div className={style.containerProduct}>
-        {product.name}
-    <img className={style.image} src={product.image}  />
+       <p className={style.textProduct + ' ' + style.unselectable}>{product.name}</p>
+
+        <Swiper cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]} className={style.mySwiper}>
+        {product.image.map((imagesrc, index) => (
+          
+          <SwiperSlide className={style.swiperSlide} key={index}> <div style={{ backgroundImage: `url(${imagesrc})` }} className={style.slideBackground}> </div> </SwiperSlide>
+         ))}
+      </Swiper>
+      {product.price}
+     <div className={style.ContainertextProduct}>
+
+      <button className={style.icontextProduct}>
+        Ver Prodcuto <FaRegEye/>
+      </button>
+      <button className={style.icontextProduct}>
+        Añadir al Carrito <FaCartPlus/>
+      </button>
+
+     </div>
+    
+ 
     
     </div>
   )
